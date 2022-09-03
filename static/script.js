7da898dc19621dc93cf6c41ad10d37878a0ec82a
ieatramen <76416193+Nab728@users.noreply.github.com>
2022-09-02 21:48:00 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -7,19 +7,21 @@ let xhr;
 var title;
 var artist;
 let playerOneTurn = true;
+let start;
+let isClientWorking = true;
 //load youtube api
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-let start;
+
 //youtbe player variables
 var player;
 var isWorking = false;
 var done = false;
 
 //url change to your domain of the website
-var redirect_uri = "https://nab728.github.io/SongQuiz/"; //switch to the html page you are 
+var redirect_uri = "http://127.0.0.1:5501/templates/index.html"; //switch to the html page you are 
 var client_id;
 var client_secret;
 
@@ -28,115 +30,6 @@ const TOKEN = "https://accounts.spotify.com/api/token";
 var access_token = null;
 var refresh_token = null;
 
-function onPageLoad(){
-    client_id = localStorage.getItem("client_id");
-    client_secret = localStorage.getItem("client_secret");
-    if ( window.location.search.length > 0 )
-    {
-        handleRedirect();
-    }
-    else
-    {
-        access_token = localStorage.getItem("access_token");
-        if ( access_token == null ){
-            // we don't have an access token so present token section
-            document.getElementById("startPage").style.display = 'flex';  
-        }
-        else {
-            // we have an access token so present device section
-            document.getElementById("gamePage").style.display = 'block';
-            fetch("./topHits.json")           
-            .then((result) => {
-                
-                return result.json()
-            }).then((data) => {
-                songs = data.items 
-                startSongPlayer() 
-            });
-        }
-       
-    }
-}
-function handleRedirect(){
-    let code = getCode();
-    fetchAccessToken( code );
-    window.history.pushState("", "", redirect_uri); // remove param from url
-}
-function fetchAccessToken(code){
-    let body = "grant_type=authorization_code";
-    body += "&code=" + code; 
-    body += "&redirect_uri=" + encodeURI(redirect_uri);
-    body += "&client_id=" + client_id;
-    body += "&client_secret=" + client_secret;
-    callAuthorizationApi(body);
-}
-function callAuthorizationApi(body){
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", TOKEN, true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
-    xhr.send(body);
-    xhr.onload = handleAuthorizationResponse;
-}
-
-function getCode(){
-    let code = null;
-    const queryString = window.location.search;
-    if ( queryString.length > 0 ){
-        const urlParams = new URLSearchParams(queryString);
-        code = urlParams.get('code')
-    }
-    return code;
-}
-function handleAuthorizationResponse(){
-    
-    if ( this.status == 200 ){
-        var data = JSON.parse(this.responseText);
-        console.log("data is" + data);
-        if ( data.access_token != undefined ){
-            access_token = data.access_token;
-            localStorage.setItem("access_token", access_token);
-        }
-        if ( data.refresh_token  != undefined ){
-            refresh_token = data.refresh_token;
-            localStorage.setItem("refresh_token", refresh_token);
-        }
-        onPageLoad();
-    }
-    else {
-        console.log(this.responseText);
-        alert(this.responseText);
-    }
-}
-function refreshAccessToken(){
-    refresh_token = localStorage.getItem("refresh_token");
-    let body = "grant_type=refresh_token";
-    body += "&refresh_token=" + refresh_token;
-    body += "&client_id=" + client_id;
-    callAuthorizationApi(body);
-}
-
-function requestAuthorization()
-{
-    let url = AUTHORIZE;
-    client_id = document.getElementById("clientId").value;
-    client_secret = document.getElementById("clientSecret").value;
-    localStorage.setItem("client_id", client_id);
-    localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
-    url += "?client_id=" + client_id;
-    url += "&response_type=code";
-    url += "&redirect_uri=" + encodeURI(redirect_uri);
-    url += "&show_dialog=true";
-    window.location.href = url; // Show Spotify's authorization screen
-}
-function checkTurn()
-{
-    if(playerOneTurn)
-    {
-        return 1;
-    }
-    return 2;
-}
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector("#form").addEventListener('submit', function(e) {
         let answer = document.querySelector("#songTitle").value;
@@ -181,7 +74,113 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 });
 
+function onPageLoad(){
+    client_id = localStorage.getItem("client_id");
+    client_secret = localStorage.getItem("client_secret");
+    if ( window.location.search.length > 0 )
+    {
+        handleRedirect();
+    }
+    else
+    {
+        access_token = localStorage.getItem("access_token");
+        if ( access_token == null ){
+            // we don't have an access token so present token section
+            document.getElementById("startPage").style.display = 'flex';  
+        }
+        else {
+            // we have an access token so present device section
+            document.getElementById("gamePage").style.display = 'block';
+            fetch("./topHits.json")           
+            .then((result) => {
+                return result.json()
+            }).then((data) => {
+                songs = data.items 
+                startSongPlayer() 
+            });
+        }
+       
+    }
+}
+function handleRedirect(){
+    let code = getCode();
+    fetchAccessToken( code );
+    window.history.pushState("", "", redirect_uri); // remove param from url
+}
+function fetchAccessToken(code){
+    let body = "grant_type=authorization_code";
+    body += "&code=" + code; 
+    body += "&redirect_uri=" + encodeURI(redirect_uri);
+    body += "&client_id=" + client_id;
+    body += "&client_secret=" + client_secret;
+    callAuthorizationApi(body);
+}
+function callAuthorizationApi(body){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", TOKEN, true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('Authorization', 'Basic ' + btoa(client_id + ":" + client_secret));
+    xhr.send(body);
+    xhr.onload = handleAuthorizationResponse;
+}
 
+function getCode(){
+    let code = null;
+    const queryString = window.location.search;
+    if ( queryString.length > 0 ){
+        const urlParams = new URLSearchParams(queryString);
+        code = urlParams.get('code')
+    }
+    return code;
+}
+function handleAuthorizationResponse(){
+    
+    if ( this.status == 200 ){
+        var data = JSON.parse(this.responseText);
+        if ( data.access_token != undefined ){
+            access_token = data.access_token;
+            localStorage.setItem("access_token", access_token);
+        }
+        if ( data.refresh_token  != undefined ){
+            refresh_token = data.refresh_token;
+            localStorage.setItem("refresh_token", refresh_token);
+        }
+        onPageLoad();
+    }
+    else {
+        console.log(this.responseText);
+        alert(this.responseText);
+    }
+}
+function refreshAccessToken(){
+    refresh_token = localStorage.getItem("refresh_token");
+    let body = "grant_type=refresh_token";
+    body += "&refresh_token=" + refresh_token;
+    body += "&client_id=" + client_id;
+    callAuthorizationApi(body);
+}
+
+function requestAuthorization()
+{
+    let url = AUTHORIZE;
+    client_id = document.getElementById("clientId").value;
+    client_secret = document.getElementById("clientSecret").value;
+    localStorage.setItem("client_id", client_id);
+    localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
+    url += "?client_id=" + client_id;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(redirect_uri);
+    url += "&show_dialog=true";
+    window.location.href = url; // Show Spotify's authorization screen
+}
+function checkTurn()
+{
+    if(playerOneTurn)
+    {
+        return 1;
+    }
+    return 2;
+}
 function formatString(s)
 {
     if(s != undefined)
@@ -229,21 +228,33 @@ function processRequest()
         title  = result["name"];
         artist = result["artists"][0]["name"];
     }
-    else
+    else if (xhr.status == 401)
     {
-        refreshAccessToken();
+        alert("access token has expired. Returning to home page.");
+        
+        if (client_id != "" && client_secret != "" && refresh_token != "" && refresh_token != undefined)
+        {
+            refreshAccessToken();
+            isClientWorking = true;
+        }
+        else
+        {
+            document.getElementById("gamePage").style.display = 'none';
+            document.getElementById("startPage").style.display = 'flex';
+            isClientWorking = false;
+        }
+        
+            
+        //alert("Error replace you access_token");
     }
 }
 function replay()
 { 
     var videoPlayer = document.querySelector('#video-container');
-    console.log(videoPlayer.src);
     videoPlayer.src = videoPlayer.src + "&start=" + start + "&end=" + (start + 10);
-    console.log(videoPlayer.src);
 }
 function chooseSong()
 {
-    console.log(songs);
     let index = Math.floor(Math.random() * songs.length); 
     while (prevSongs.includes(index))
     {
@@ -258,18 +269,9 @@ function chooseSong()
     }
     return index;
 }
-let firstTime = true;
 function onYouTubeIframeAPIReady() 
 {
     isWorking = true;
-    //console.log("Youtube iframe works");
-    if (firstTime && songs != null)
-    { 
-        //startSongPlayer();
-        firstTime = false;
-        
-    }
-    
 }
 function createPlayer(id)
 {
@@ -279,11 +281,13 @@ function createPlayer(id)
         {
             player.destroy();
         }
-        player = new YT.Player("video-container", {
+        player = new YT.Player("video", {
         height: 300,
         width: 300,
         videoId: id,
         playerVars: {
+            'fs' : 0,
+            'controls': 0,
             'playsinline': 1
             },
             events: {
@@ -302,7 +306,11 @@ function startSongPlayer()
     //start is undefined in the beginning for some reason but it still works so ig that's ok
     //0 to length of the song - 20 seconds (so we don't play the end of the song)
     start = Math.floor(Math.random() * (150 - 20 + 1)) + 20;
-    createPlayer(songs[songIndex].snippet.resourceId.videoId, start);
+    if (isClientWorking)
+    {
+        createPlayer(songs[songIndex].snippet.resourceId.videoId, start);
+    }
+    
 }
 //set start of clip and play the video
 function onPlayerReady(event) 
@@ -315,7 +323,7 @@ function onPlayerStateChange(event) {
     if(event.data == YT.PlayerState.PLAYING)
     {
        document.querySelector('.wrapper').style.pointerEvents = "none";
-      // document.querySelector('.wrapper').style.display = "none";
+       //document.querySelector('.wrapper').style.display = "none";
        
        setTimeout(stopVideo, 10000);
     
